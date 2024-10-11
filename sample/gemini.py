1deeba3b9bddeb523b12fb1d62f862b758571446
jbstuart01 <145608139+jbstuart01@users.noreply.github.com>
2024-10-11 15:14:14 -0400

    Tweak response guidelines

--- sample/gemini.py
+++ sample/gemini.py
@@ -29,7 +29,7 @@ def clean_query(query):
 # convert natural language to an SQLite query
 def convert_language_to_sql(prompt):
     # instructions for the converter model
-    instruction = '''Your job is to convert a natural language question to an SQLite query. Use the following database schema to construct the queries. When building the query, always use "SELECT *" so that as much information is received as possible. Always JOIN PlayerStats and GameInfo to get the most information as possible. JOIN multiple tables together if necessary. Provide no other output other than the final SQL query.
+    instruction = '''Your job is to convert a natural language question to an SQLite query. Use the provided database schema to make the queries. When building the query, always use "SELECT *" so that as much information is received as possible. Always JOIN PlayerStats and GameInfo to get the most information as possible. JOIN multiple tables together if necessary. Provide no other output other than the final SQL query.
             SCHEMA:
             CREATE TABLE IF NOT EXISTS Players (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Number INT, HomeCity TEXT, HomeState TEXT, Height TEXT, Weight INT, BirthYear INT, BirthMonth INT, BirthDay INT)
             CREATE TABLE IF NOT EXISTS PlayerStats (id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, Team TEXT, Name TEXT, Minutes INT, FGM INT, FGA INT, TFGM INT, TFGA INT, FTM INT, FTA INT, ORB INT, DRB INT, TRB INT, PF INT, AST INT, STL INT, BLK INT, TOV INT, PTS INT)
@@ -48,7 +48,7 @@ def results_cleaner(question, answer):
         QUESTION: <question the user asked>
         ANSWER: <answer to the user's question>
         
-        Provide a concise response that communicates the answer to the user's question. Do NOT respond with JSON data. You respond with poor grammar and no capitalization or punctuation.
+        Provide a response that communicates the answer to the user's question. Do NOT respond with JSON data. Always include the player name, opponent, and date when possible. You respond with poor grammar and no capitalization. Convert dates to string format. Use as much of the provided data as possible to make your response.
     '''
     
     # create the model
@@ -76,9 +76,9 @@ def main():
         sql_response = "None"
         
     # print the final response
-    print(f"Query: {sql_query}")
-    print(f"Answer: {sql_response}")
-    print(f"Natural response: {results_cleaner(prompt, sql_response)}")
-    
+    #print(f"Query: {sql_query}")
+    #print(f"Answer: {sql_response}")
+    #print(f"Natural response: {results_cleaner(prompt, sql_response)}")
+    print(results_cleaner(prompt, sql_response))
 if __name__ == "__main__":
     main()
