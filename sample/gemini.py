809635b425a5ffb9e07bbc00973b115bee2be1f2
jbstuart01 <145608139+jbstuart01@users.noreply.github.com>
2024-10-16 12:53:35 -0400

    Refine instructions for Gemini

--- sample/gemini.py
+++ sample/gemini.py
@@ -9,6 +9,12 @@ with open('data/api_key.yaml', 'r') as file:
     config = yaml.safe_load(file)
     API_KEY = config.get('api_key')
     
+# define the database schema
+SCHEMA = '''CREATE TABLE IF NOT EXISTS Players (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Number INT, HomeCity TEXT, HomeState TEXT, Height TEXT, Weight INT, BirthYear INT, BirthMonth INT, BirthDay INT)
+            CREATE TABLE IF NOT EXISTS PlayerStats (id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, Team TEXT, Name TEXT, Minutes INT, FGM INT, FGA INT, TFGM INT, TFGA INT, FTM INT, FTA INT, ORB INT, DRB INT, TRB INT, PF INT, AST INT, STL INT, BLK INT, TOV INT, PTS INT)
+            CREATE TABLE GameInfo (id INTEGER PRIMARY KEY AUTOINCREMENT, Date DATE, Day TEXT, Season TEXT, Opponent TEXT, Result TEXT, Location TEXT, UKScore INT, OppScore INT, Notes TEXT, Arena TEXT, Attendance INT, City TEXT, UKHalfScore INT, OppHalfScore INT, OTS INT, UKRank INT, OppRank INT, OppCoach TEXT, OppConference TEXT, LeadScorer TEXT, LeadScorerPoints INT, LeadRebounder TEXT, LeadRebounderRebs INT, LeadAssister TEXT, LeadAssisterAsts INT, LeadStealer TEXT, LeadStealerSteals INT, LeadBlocker TEXT, LeadBlockerBlocks INT)
+        '''
+    
 # create an AI model
 def create_model(instruction):
     genai.configure(api_key = API_KEY)
@@ -29,12 +35,8 @@ def clean_query(query):
 # convert natural language to an SQLite query
 def convert_language_to_sql(prompt):
     # instructions for the converter model
-    instruction = '''Your job is to convert a natural language question to an SQLite query. Use the provided database schema to make the queries. When building the query, be very liberal with how much data you SELECT. Always JOIN PlayerStats and GameInfo to get the most information as possible. JOIN multiple tables together if necessary. Provide no other output other than the final SQL query.
-            SCHEMA:
-            CREATE TABLE IF NOT EXISTS Players (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Number INT, HomeCity TEXT, HomeState TEXT, Height TEXT, Weight INT, BirthYear INT, BirthMonth INT, BirthDay INT)
-            CREATE TABLE IF NOT EXISTS PlayerStats (id INTEGER PRIMARY KEY AUTOINCREMENT, Date TEXT, Team TEXT, Name TEXT, Minutes INT, FGM INT, FGA INT, TFGM INT, TFGA INT, FTM INT, FTA INT, ORB INT, DRB INT, TRB INT, PF INT, AST INT, STL INT, BLK INT, TOV INT, PTS INT)
-            CREATE TABLE GameInfo (id INTEGER PRIMARY KEY AUTOINCREMENT, Date DATE, Day TEXT, Season TEXT, Opponent TEXT, Result TEXT, Location TEXT, UKScore INT, OppScore INT, Notes TEXT, Arena TEXT, Attendance INT, City TEXT, UKHalfScore INT, OppHalfScore INT, OTS INT, UKRank INT, OppRank INT, OppCoach TEXT, OppConference TEXT, LeadScorer TEXT, LeadScorerPoints INT, LeadRebounder TEXT, LeadRebounderRebs INT, LeadAssister TEXT, LeadAssisterAsts INT, LeadStealer TEXT, LeadStealerSteals INT, LeadBlocker TEXT, LeadBlockerBlocks INT)
-        '''
+    instruction = '''Your job is to convert a natural language question to an SQLite query. Use the provided database schema to make the queries. When building the query, always use "SELECT *" to get as much data as possible. Always JOIN PlayerStats and GameInfo to get the most information as possible. JOIN multiple tables together if necessary. Provide no other output other than the final SQL query.
+           '''
     # create the model
     model = create_model(instruction)
     
@@ -42,16 +44,17 @@ def convert_language_to_sql(prompt):
     return clean_query(model.generate_content(prompt).text)
 
 # presents the results in conversational format
-def results_cleaner(question, answer):
+def results_cleaner(question, query, answer):
     if answer == "None":
         return "No response possible."
     else:
         # instructions for the response model
         instruction = '''You will receive a prompt in the format:
             QUESTION: <question the user asked>
-            ANSWER: <answer to the user's question>
+            QUERY: <SQL query used to fetch results>
+            RESPONSE: <results of running SQL query>
             
-            Provide a response that communicates the answer to the user's question. Do NOT respond with JSON data. Convert dates to string format. Assume that the user always wants to know as much information about the game(s) as possible.
+            Provide a response that combines QUESTION and ANSWER into a valid conversational statement. Include as much data from ANSWER as possible.
         '''
         
         # create the model
@@ -77,9 +80,10 @@ def main():
     except:
         print("Something went wrong when running the query. Please try again.")
         sql_response = "None"
-        
+    
+    print(f"Query: {sql_query}")
     # print the final response
-    print(results_cleaner(prompt, sql_response))
+    print(f"Response: {results_cleaner(prompt, sql_query, sql_response)}")
     
 if __name__ == "__main__":
     main()
